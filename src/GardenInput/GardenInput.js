2c555c0e4c662180e09f8f83c7f4bd3c00431d8c
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-09 12:44:53 -0700

    Added a form on the main page for input

---
+++ src/GardenInput/GardenInput.js
@@ -0,0 +1,73 @@
+import './GardenInput.css';
+import React, { useState } from 'react';
+
+function GardenInput() {
+  const [gardenInfo, setGardenInfo] = useState({
+    name: '',
+    hardiness_zone: '',
+    sunlight: '',
+    soil_type: '',
+    water_needs: '',
+    purpose: '',
+  });
+
+  return (
+    <section>
+      <h1>Input Garden Info</h1>
+      <form>
+      <label>
+          Garden Name:
+          <input
+            type="text"
+            value={gardenInfo.name}
+          />
+        </label>
+        <br />
+        <label>
+          Soil Type:
+          <input 
+          type="text"
+          value={gardenInfo.soil_type}
+          />
+        </label>
+        <br/>
+        <label>
+          Hardiness Zone:
+          <input
+            type="text"
+            value={gardenInfo.hardiness_zone}
+          />
+        </label>
+        <br />
+        <label>
+          Sunlight:
+          <input
+            type="text"
+            value={gardenInfo.sunlight}
+          />
+        </label>
+        <br />
+        <label>
+          Water Needs:
+          <input
+            type="text"
+            value={gardenInfo.water_needs}
+          />
+        </label>
+        <br />
+        <label>
+          Purpose:
+          <input
+            type="text"
+            value={gardenInfo.purpose}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+    
+  );
+};
+
+export default GardenInput
