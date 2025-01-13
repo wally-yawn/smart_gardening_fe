a5cf44fbdea56d0e7de6105983b5d1d58cf45b59
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-13 12:06:27 -0700

    functionality for deleting plant implimented for test plant data

--- src/Gardens/Gardens.js
+++ src/Gardens/Gardens.js
@@ -1,9 +1,22 @@
-import React from "react";
-import "./Gardens.css";
+import React, { useState, useEffect } from "react";import "./Gardens.css";
 import Plants from "../Plants/Plants";
 
 function Gardens({ gardens }) {
-  const allPlants = gardens.plants.map((plant) => {
+  const [allPlants, setAllPlants] = useState(gardens.plants);
+
+  const testPlants = [
+    { id: 1, name: "Tomato", img_url: "https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg", description: "A popular vegetable that thrives in full sun." },
+    { id: 2, name: "Carrot", img_url: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Carrots.JPG", description: "A root vegetable that grows well in loamy soil." },
+  ];
+
+  useEffect(() => {
+    setAllPlants(testPlants);
+  }, []);
+
+  const removePlant = (plantId) => {
+    setAllPlants(allPlants.filter((plant) => plant.id !== plantId));
+  };
+  const plantCards = allPlants.map((plant) => {
     return (
       <Plants
         id={plant.id}
@@ -11,6 +24,7 @@ function Gardens({ gardens }) {
         name={plant.name}
         image={plant.img_url}
         description={plant.description}
+        deletePlant={removePlant}  
       />
     );
   });
@@ -18,8 +32,8 @@ function Gardens({ gardens }) {
   return (
     <div>
       <section className="my-garden-page">
-        <h2 class="garden-name">{gardens.name}</h2>
-        <div class="all-plant-cards">{allPlants}</div>
+        <h2 className="garden-name">{gardens.name}</h2>
+        <div className="all-plant-cards">{plantCards}</div>
       </section>
     </div>
   );
