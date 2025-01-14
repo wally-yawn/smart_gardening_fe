6f0413c1686deb46be739ef1775bf78e428556be
sethverrill <verrill.seth@gmail.com>
2025-01-14 14:33:05 -0700

    add save and edit logic to existing GardenInput

--- src/GardenInput/GardenInput.js
+++ src/GardenInput/GardenInput.js
@@ -14,6 +14,8 @@ function GardenInput({ gardenId, setRecommendations }) {
     purpose: "",
   });
 
+  const [hasGarden, setHasGarden] = useState(false);
+
   useEffect(() => {
     const fetchGarden = async () => {
       try {
@@ -28,6 +30,7 @@ function GardenInput({ gardenId, setRecommendations }) {
             water_needs: gardenData.water_needs || "",
             purpose: gardenData.purpose || "",
           });
+          setHasGarden(true);
         } else {
           console.error("Failed to fetch garden data", response.status);
         }
@@ -74,6 +77,35 @@ function GardenInput({ gardenId, setRecommendations }) {
       })
       .catch((error) => setError(error.message));
   }
+
+  const handleSaveOrEdit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const method = hasGarden ? "PATCH" : "POST";
+      const saveUrl = hasGarden
+      ? `${url}gardens/1`
+      : `${url}gardens`;
+
+      const response = await fetch(saveUrl, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ garden: gardenInfo }),
+      });
+
+      if(response.ok) {
+        const gardenData = await response.json();
+        setHasGarden(true);
+        console.log("Garden saved/updated successfully: ", gardenData);
+      } else {
+        console.error("Failed to save/update garden");
+      }
+    } catch (error) {
+      console.error("Failed to save/update garden", error.message)
+    }
+  };
 
   return (
     <section className="garden-form-section">
@@ -172,6 +204,7 @@ function GardenInput({ gardenId, setRecommendations }) {
           </label>
         </div>
         <button type="submit" onClick={searchRecommendations}>Search</button>
+        <button type="button" onClick={handleSaveOrEdit}>{hasGarden ? "Edit" : "Save"}</button>
       </form>
     </section>
   );
