ded1119355df567a6897a76d1793c7d0d73a3156
devklynch <devklynch@gmail.com>
2025-01-14 16:43:06 -0700

    added useLocation to only show SavePlant on main page

--- src/Plants/Plants.js
+++ src/Plants/Plants.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Plants.css";
 import SavePlant from "../SavePlant/SavePlant";
+import { useLocation } from "react-router-dom";
 
 function Plants({ id, name, image, description }) {
-  console.log("Test", name);
+  const location = useLocation();
+
   return (
     <div className="plant-card">
       <h3>{name}</h3>
       <img src={image} alt={name} className="plant-image" />
       <p>{description}</p>
-      <SavePlant name={name} img_url={image} description={description} />
+      {location.pathname === "/" && (
+        <SavePlant name={name} imgage={image} description={description} />
+      )}
     </div>
   );
 }
