657d4bc2c502c09127d3c1908e40838835c17e18
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-15 12:29:37 -0700

    added a dynamic fetch call

--- src/Gardens/Gardens.js
+++ src/Gardens/Gardens.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";import "./Gardens.css";
 import Plants from "../Plants/Plants";
 
 function Gardens({ gardens }) {
+  const url = 'http://localhost:3000/api/v1'
   const [allPlants, setAllPlants] = useState(gardens.plants);
 
   const testPlants = [
@@ -15,7 +16,7 @@ function Gardens({ gardens }) {
 
   const removePlant = async (plantId) => {
     try {
-      const response = await fetch(`http://loclhost300/garden/${gardens.id}/plant/${plantId}`, {
+      const response = await fetch(`${url}/gardens/1/plants/${plantId}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
