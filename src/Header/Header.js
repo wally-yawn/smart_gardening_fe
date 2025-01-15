5b0fda4a30179fe9817703596444458cb5b4ab8b
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-14 17:08:46 -0700

    jst added a quick css to get the button and title on the same line, make sure css worked

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 
 const Header = () => {
   const navigate = useNavigate();
-  const page = useLocation(); // Use useLocation to get the current path
-  const mainPage = page.pathname === "/"; // Check if it's the main page
+  const page = useLocation();
+  const mainPage = page.pathname === "/"; 
 
   const goHome = () => {
     navigate('/');
@@ -14,7 +14,7 @@ const Header = () => {
   return (
     <header className="header">
       <h1 className="header-title">Smart Gardening</h1>
-      {/* Only show the home button if it's not the main page */}
+
       {!mainPage && (
         <button onClick={goHome} className="home-btn">
           Go Home
