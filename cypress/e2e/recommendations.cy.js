69ce9ac6a66745424c90444f58ab6f99656c9cf4
Wally Wallace <jrvw001@gmail.com>
2025-01-15 19:09:03 -0700

    Favicon for the win

--- cypress/e2e/recommendations.cy.js
+++ cypress/e2e/recommendations.cy.js
@@ -7,6 +7,10 @@ describe('recommendations', () => {
     cy.intercept('GET', 'http://localhost:3000/api/v1/recommendation?zip_code=80209&sunlight=Full+Sun&soil_type=Loamy&water_needs=High&purpose=Food+Production', {
       fixture: 'recommendations'
     })
+
+    cy.intercept('GET', 'http://localhost:3000/api/v1/recommendation?zip_code=80221&sunlight=Shade+Sun&soil_type=Silty&water_needs=Moderate&purpose=Recreation', {
+      fixture: 'recommendations_2'
+    })
     cy.visit('http://localhost:3001/')
     
   })
@@ -15,7 +19,7 @@ describe('recommendations', () => {
     cy.get('h1').contains('Smart Gardening')
     .get('.my-garden-button').should('exist')
     .get('.garden-form-section > h1').contains('Input Garden Info')
-    .get('.form-row > :nth-child(1) > :nth-child(1)').should('have.value','80209')
+    .get('input[name="zip_code"]').should('have.value','80209')
     .get('select[name="name"]').should('have.value', 'Vegetable Garden')
     .get('select[name="soil_type"]').should('have.value', 'Loamy')
     .get('select[name="sunlight"]').should('have.value', 'Full Sun')
@@ -25,7 +29,8 @@ describe('recommendations', () => {
     .get('.default-message').contains('Enter your garden information and click Search to get recommendations!')
   })
 
-  it('shows recommendations after clicking Search', () => {
+  it('shows recommendations after clicking Search with the default garden', () => {
+    /*put in selecting the attributes*/
     cy.get('.search-button').click()
     .get('.plant-cards').find('.plant-card').should('have.lengthOf', 2)
     .get('.plant-cards > :nth-child(1) > h3').contains('Strawberry')
@@ -36,6 +41,22 @@ describe('recommendations', () => {
     .get('.plant-cards > :nth-child(2) > img').should('have.attr', 'src', 'https://example.com/mock-basil.jpg')
     .get('.plant-cards > :nth-child(2) > img').should('have.attr', 'alt', 'Basil')
     .get('.plant-cards > :nth-child(2) > p').contains('Aromatic herb thriving in full sun with moderate watering.')
+  })
 
+  it('shows recommendations after clicking Search with the selected attributes', () => {
+    cy.get('.search-button').click()
+    .get('.plant-cards').find('.plant-card').should('have.lengthOf', 3)
+    .get('.plant-cards > :nth-child(1) > h3').contains('Catnip')
+    .get('.plant-cards > :nth-child(1) > img').should('have.attr', 'src', 'https://example.com/mock-catnip.jpg')
+    .get('.plant-cards > :nth-child(1) > img').should('have.attr', 'alt', 'Catnip')
+    .get('.plant-cards > :nth-child(1) > p').contains("Did you know this isn't a small plant? Cuz I didn't when I planted it")
+    .get('.plant-cards > :nth-child(2) > h3').contains('Rosemary')
+    .get('.plant-cards > :nth-child(2) > img').should('have.attr', 'src', 'https://example.com/mock-rosemary.jpg')
+    .get('.plant-cards > :nth-child(2) > img').should('have.attr', 'alt', 'Rosemary')
+    .get('.plant-cards > :nth-child(2) > p').contains('Aromatic herb thriving in full sun with moderate watering.')
+    .get('.plant-cards > :nth-child(3) > h3').contains('Beets')
+    .get('.plant-cards > :nth-child(3) > img').should('have.attr', 'src', 'https://example.com/mock-beets.jpg')
+    .get('.plant-cards > :nth-child(3) > img').should('have.attr', 'alt', 'Beets')
+    .get('.plant-cards > :nth-child(3) > p').contains("Who doesn't love beets?")
   })
 })
